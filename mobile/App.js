7f53a8ee928ebb4a1f6079a7d579682f54c297bb
Nícolas Suzigan <nicolas_suzigan@hotmail.com>
2020-03-27 00:31:25 -0300

    Mobile do app

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+//importar rotas
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />  
+  );
+}
